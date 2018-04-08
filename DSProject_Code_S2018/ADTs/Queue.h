8b22c2b00313e87e570495e33d9bce626f7d4415
AbdElRahman ElGamil (Jimmy) <abdurrumanmohamed@gmail.com>
2018-04-08 10:59:57 +0000

    Queue.h edited online with Bitbucket

--- DSProject_Code_S2018/ADTs/Queue.h
+++ DSProject_Code_S2018/ADTs/Queue.h
@@ -42,7 +42,7 @@ bool Queue<T>::enqueue(const T& item) {
 }
 
 template<typename T>
-bool Queue<T>::isEmpty() const {
+bool Queue<T>::isEmpty() const{
 	if (head == nullptr)
 		return true;
 	return false;
