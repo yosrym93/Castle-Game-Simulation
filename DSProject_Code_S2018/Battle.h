7261f3bdbe1cbbe56072e4b6d6d8ff3ae71121d5
Yosry Mohammad <yosrym93@gmail.com>
2018-05-03 00:43:59 +0200

    removed updateAsync() func

--- DSProject_Code_S2018/Battle.h
+++ DSProject_Code_S2018/Battle.h
@@ -66,7 +66,6 @@ public:
 	void silentTime();
 	
 	/***************************************************************************************/
-	void updateAsync();				//Calls update function asynchronously
 	void killRandom();				//Kills enemieas randomly according to phase 1.
 	void update(int);				//Updates all lists and the GUI array
 	void load(GUI*);				//Loads all enemies to the inactive list
